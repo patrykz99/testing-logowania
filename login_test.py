33e02a1c61f7b52baced86ccab860b3e0292c531
Patryk Zioło <patryk.ziolo12@gmail.com>
2025-01-24 15:47:43 +0100

    Add main script to tests executions locally

---
+++ login_test.py
@@ -0,0 +1,56 @@
+import unittest,time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from pages.login_pg import LoginPage
+from pages.store_pg import InventoryPage
+
+
+class loginTests(unittest.TestCase):
+
+    def setUp(self):
+        
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        self.driver.maximize_window()
+
+        self.login_page = LoginPage(self.driver)
+        self.login_page.open()
+
+    def tearDown(self):
+        time.sleep(2)
+        self.driver.quit()
+
+    def test_valid_form(self):
+        self.login_page.login('standard_user', 'secret_sauce')
+        inventory_page = InventoryPage(self.driver)
+
+        self.assertTrue(inventory_page.is_loaded())
+
+    def test_invalid_form(self):
+        self.login_page.login('xyz', 'def4ault')
+        
+        error_message = self.login_page.find(self.login_page.err_message).text
+        self.assertIn('Username and password do not match any user in this service', error_message)
+
+    def test_empty_form(self):
+        self.login_page.login('', '')
+
+        error_message = self.login_page.find(LoginPage.err_message).text
+        self.assertIn('Username is required', error_message)
+    
+    def test_empty_login(self):
+        self.login_page.login('', 'secret_sauce')
+
+        error_message = self.login_page.find(LoginPage.err_message).text
+        self.assertIn('Username is required', error_message)
+          
+    def test_empty_password(self):
+        self.login_page.login('problem_user', '')
+
+        error_message = self.login_page.find(LoginPage.err_message).text
+        self.assertIn('Password is required', error_message)
+
+
+if __name__ == "__main__":
+    unittest.main()
